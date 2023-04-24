37873eb5502c7ce70213ad7942135f3024165c04
Jonas <57955592+JonasHeinemann@users.noreply.github.com>
2023-04-24 13:48:06 +0200

    Initalise project (#1)
    * Initalise project
    
    * apply review suggestions to GetUri func
    
    * fix Sprintf string
    
    * move const variable and remove unused variables
    
    * Add response type for addedrows
    
    * replace interfaces with any alias
    
    * initilise resty client inside the lib itself to not for the dependency directly on the user
    
    * update README
    
    * add test case and fix token check
    
    * remove timeout
    
    * change how the return value is filled in sendApiRequest + remove unused variable
    
    * code review change
    
    ---------
    
    Co-authored-by: Jonas Heinemann <jonas.heinemann@clarilab.de>

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# zoholab
-zoholab is a go libary that can be used to call the zoho API.
+# 🔍 zoholab
+
+zoholab is a go libary that can be used to call the zoho API. The motivation to do this project stems from an issue we ran into while developing a way to track data in zoho analytics via their provided go client.
+While the client does work there were major improvements that needed to be made for us to be able to use it in our code. This is an improved take of this client with the feartures that we currently need, made with the intent of improving the go client for zoho.
+
+## 💡 Improvements over offical zoho go client
+
+* Added a service struct
+* Replace net/http calls with resty calls
+* Remove panics from code
+* Replace throwing of ServerExceptions with standard error handling
+* Removed global variables
+
+## 👷‍♀️ Features
+
+Currently you are able to add a row to a table within zoho analytics. Some new functions might follow if needed.
+
+## 🤯 Usage
+
+Before using: Follow the steps in the [zoho analytics documentation](https://www.zoho.com/analytics/api/#prerequisites) to create a new client and your refresh token. Make sure you create a table that you want to add your data into.
+
+### ✅ You will need
+* Client ID
+* Client Secret
+* Refresh token
+* Email address in which the workspace is in
+* Workspace name
+* Table name
+
+Make sure the client that you have created has the correct permissions to access the table that you want to modify.
+
+### Installation
+
+```
+go get github.com/Clarilab/zoholab
+```
+
+### 🛳 Import
+
+```go
+import "github.com/Clarilab/zoholab"
+```
+
+### 😍 Initilise zoholab
+
+```go
+func main() {
+	zohoService := zoho.NewZohoService()
+
+	zohoService.SetServiceParams(Clientid, Clientsecret, Refreshtoken)
+}
+ ```
+
+### 🚣  Code Example for adding a row
+
+```go
+func addRow(zohoService *zoholab.ZohoService) error {
+	const errMessage = "could not add row"
+
+	url := zoholab.GetUri(email, workspace, tbname)
+
+	columnvalues := map[string]string{
+		"your column name": "your column entry",
+	}
+
+	addedrows, err := zoholab.AddRow(url, columnvalues)
+	if err != nil {
+		return errors.Wrap(err, errMessage)
+	}
+
+	return nil
+}
+ ```
+
+* Call this function in your main.go and pass in the zoho instance.
+
+```go
+addRow(zohoService)
+```
+
+* This is just an example you do not need to make an extra function to call in your main.go, just makes it a little bit cleaner.
---
+++ zoho.go
@@ -0,0 +1,111 @@
+package zoholab
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Clarilab/zoholab/domain"
+	"github.com/Clarilab/zoholab/middlewares"
+	"github.com/go-resty/resty/v2"
+	"github.com/pkg/errors"
+)
+
+const (
+	reportsUri   = "https://analyticsapi.zoho.eu"
+	validJson    = "true"
+	outputFormat = "JSON"
+	errorFormat  = "JSON"
+	apiVersion   = "1.0"
+	addRow       = "ADDROW"
+)
+
+// ZohoService is the struct for the zoho service.
+type ZohoService struct {
+	restyClient *resty.Client
+}
+
+// NewZohoService instantiates a new zoho service.
+func NewZohoService() *ZohoService {
+	return &ZohoService{
+		restyClient: resty.New(),
+	}
+}
+
+// SetServiceParams sets the params needed to call the zoho api.
+func (s *ZohoService) SetServiceParams(clientID, clientSecret, refreshToken string) {
+	authTokenMiddleware := middlewares.NewAuthTokenMiddleware(clientID, clientSecret, refreshToken)
+
+	s.restyClient.OnBeforeRequest(authTokenMiddleware.AddAuthTokenToRequest)
+}
+
+// GetUri returns the URI for the specified table in the Zoho Analytics account.
+func (s *ZohoService) GetUri(emailID, dbName, tbName string) string {
+	// Join path is not used because the path must not be escaped.
+	return fmt.Sprintf("%s/api/%s/%s/%s", reportsUri, emailID, urlSplCharReplace(dbName), urlSplCharReplace(tbName))
+}
+
+// AddRow adds row to the specified table identified by the URI.
+func (s *ZohoService) AddRow(tableUri string, columnValues map[string]string) (*ZohoAddRowResponse, error) {
+	const errMessage = "could not add row in zoho"
+
+	var addedRows ZohoAddRowResponse
+
+	err := s.sendAPIRequest(columnValues, tableUri, addRow, &addedRows)
+	if err != nil {
+		return nil, errors.Wrap(err, errMessage)
+	}
+
+	return &addedRows, nil
+}
+
+// sendAPIRequest sends a request to the zoho api.
+func (s *ZohoService) sendAPIRequest(config map[string]string, path, action string, result any) error {
+	const errMsg = "could not send api request"
+
+	resp, err := s.restyClient.
+		R().
+		SetHeader("User-Agent", "ZohoAnalytics GoLibrary").
+		SetQueryParams(map[string]string{
+			"ZOHO_ACTION":        action,
+			"ZOHO_OUTPUT_FORMAT": outputFormat,
+			"ZOHO_ERROR_FORMAT":  errorFormat,
+			"ZOHO_API_VERSION":   apiVersion,
+			"ZOHO_VALID_JSON":    validJson,
+		}).
+		SetQueryParams(config).
+		SetResult(result).
+		Post(path)
+	if err != nil {
+		return errors.Wrap(err, errMsg)
+	}
+
+	if resp.IsError() {
+		return errors.Wrap(domain.FillApiError(resp.Body()), errMsg)
+	}
+
+	return nil
+}
+
+// Internally used. For handling special character's in the workspace name or table name.
+func urlSplCharReplace(value string) string {
+	value = strings.Replace(value, "/", "(/)", -1)
+	return strings.Replace(value, "\\", "(//)", -1)
+}
+
+// ZohoAddRowResponse is the response that the zoho api returns.
+type ZohoAddRowResponse struct {
+	Response *ZohoResponse `json:"response"`
+}
+
+// ZohoResponse is part of the ZohoAddRowResponse response Model.
+type ZohoResponse struct {
+	URI            string         `json:"uri"`
+	Action         string         `json:"action"`
+	ResponseResult ResponseResult `json:"result"`
+}
+
+// ResponseResult is part of the ZohoAddRowResponse response Model.
+type ResponseResult struct {
+	ColumnOrder []string   `json:"column_order"`
+	Rows        [][]string `json:"rows"`
+}
